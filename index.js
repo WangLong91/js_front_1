5ef0fd60b5be589a828fd4556006e3abfeba5faa
WangLong91 <85441715+WangLong91@users.noreply.github.com>
2022-11-05 12:35:02 +0600

    5 files modified

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-// Import stylesheets
-import './style.css';
+const canvasSketch = require ('canvas-sketch');
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const settings = {
+  dimensions: [1080,1080]
+};
+
+const sketch = () => {
+ let x,y,w,h;
+
+ return({context, width, height}) =>{
+  context.fillStyle = 'white';
+  context.fillRect(0,0, width, height);
+
+  x = width *0.5;
+  y = height *0.5;
+  w = width *0.6;
+  h = height *0.1;
+
+  context.save();
+  context.translate(x,y);
+
+  context.strokeStyle = "blue";
+  context.strokeRect(w *-0.5, h * -0.5, w,h);
+
+  context.begionPath();
+  context.moveTo(w*-0.5,h*-0.5);
+
+  context.restore();
+
+ };
+};
+
+canvasSketch(sketch,settings); 
